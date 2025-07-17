2cae7ad9d92a0fbca8e89042af50f18f67ee7c9c
Aziz <memoazeez@outlook.com>
2025-07-17 03:04:37 -0700

    Add about us, using Routers in header

---
+++ components/About.js
@@ -0,0 +1,23 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
+const About = () => {
+  return (
+    <div className="user-card">
+      <div>
+        {<User name={"Aziz"} ocupation={"Web developer"} location={"Dubai"} />}
+      </div>
+      <div>
+        {
+          <UserClass
+            name={"Alex"}
+            ocupation={"Web developer"}
+            location={"UK"}
+          />
+        }
+      </div>
+    </div>
+  );
+};
+
+export default About;

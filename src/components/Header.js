5317154eaecb946d91a76b5b39f0f0e7c91e0eaa
Moysser <memoazeez@outlook.com>
2025-11-06 15:34:32 +0400

    add styles in Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,48 @@
-import { LOGO_URL } from "../utils/constants";
-import { useState } from "react";
+import { LOGO_URL } from "../apidata/constants";
+import { useState, useEffect } from "react";
 import { Link } from "react-router";
 
 const Header = () => {
   const [btnName, setBtnName] = useState("login");
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="header">
+    <header
+      className={` flex justify-between transition-all duration-300 ${
+        isSticky ? "sticky top-0 bg-white shadow-md z-50" : "relative"
+      }`}
+    >
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="logo" />
+        <img className="w-25" src={LOGO_URL} alt="logo" />
       </div>
-      <div className="nav-items">
-        <ul className="list-items">
-          <li className="list-item">
+      <div className="flex self-center gap-6 pr-6 items-center">
+        <ul className="flex gap-6 justify-start">
+          <li className="font-bold hover:shadow-amber-600">
             <Link to="http://localhost:1234">Home</Link>
           </li>
-          <li className="list-item">
+          <li className="font-bold">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="list-item">
+          <li className="font-bold">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li className="list-item">Cart</li>
+          <li className="font-bold">Cart</li>
         </ul>
         <button
-          className="btn"
+          className="text-amber-50 bg-sky-500 hover:bg-sky-700 cursor-pointer p-2 rounded-sm font-bold"
           onClick={() => {
             btnName === "logout" ? setBtnName("login") : setBtnName("logout");
           }}
@@ -32,7 +50,7 @@ const Header = () => {
           {btnName}
         </button>
       </div>
-    </div>
+    </header>
   );
 };
 
